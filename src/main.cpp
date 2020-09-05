df21da40e0913a2c3d1e0e8eb3a7ad1f4f1c9bf2
Walid Amriou <walidamriou@gmail.com>
2020-09-05 15:36:14 +0200

    added tracker of the maximum and average time it take to run the loop code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
+#include <loopTimer.h>
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Track of the maximum and average time it take to run the loop code
+   loopTimer.check(&Serial);
 }
